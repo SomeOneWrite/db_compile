40c3d31d05f74cbbd2e54f7b4c315331f1dbbd83
csserf <csserf@mail.ru>
2018-10-12 18:44:45 +0500

    УСЕ ПЛОХО

--- merge.py
+++ merge.py
@@ -1,21 +1,32 @@
-# import sqlite3
-# from Helpers import without_lines
-from docx import Document
-from docx.oxml.ns import qn
-# conn = sqlite3.connect('unit_positions.sqlite')
-# query = conn.cursor()
-# new_list = []
-#
-# all_captions = list(query.execute("select * from captions").fetchall())
-# for caption in all_captions:
-#     tmp_l = []
-#     for i in caption:
-#         if type(i) is str:
-#             tmp_l.append(without_lines(i))
-#         else:
-#             tmp_l.append(i)
-#     new_list.append(tmp_l)
-#
+import sqlite3
+import re
+conn = sqlite3.connect('res.sqlite')
+query = conn.cursor()
+captions = query.execute("select * from captions").fetchall()
+
+last_razdel_id = None
+last_podrazdel_id = None
+
+for caption in captions:
+    name = caption[3]
+    parent_id = caption[2]
+    cur_id = caption[0]
+    res = re.search(r'Подраздел', name)
+    if res:
+        last_podrazdel_id = cur_id
+        if last_razdel_id:
+            query.execute("update captions set parent_id = ? where id = ?", (last_razdel_id, cur_id))
+        continue
+    res = re.search(r'Раздел', name)
+    if res:
+        last_razdel_id = cur_id
+        continue
+
+        #print("name = {}, id = {}, replaced name = {}", collection[3], collection[1], collection)
+        #query.execute("update collections set name = ? where id = ?", (caption[3].replace(".docx", ""), collection[0]))
+
+conn.commit()
+
 # for l in new_list:
 #     query.execute('update captions set name = ? where id = ?', (l[3], l[0]))
 #
